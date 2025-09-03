d9712168eda171611a3c6345a7dc15f4af76fd78
woohyeon <woohyeon6885@gmail.com>
2025-09-03 13:29:50 +0900

    feat: PostDetailResponse 추가

---
+++ src/main/java/com/hong/PostService_MCP_Server/dto/post/PostDetailResponse.java
@@ -0,0 +1,18 @@
+package com.hong.PostService_MCP_Server.dto.post;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record PostDetailResponse(
+        @JsonProperty("id") Long id,
+        @JsonProperty("title") String title,
+        @JsonProperty("content") String content,
+        @JsonProperty("writerNickname") String writerNickname,
+        @JsonProperty("createdDate") LocalDateTime createdDate,
+        @JsonProperty("lastModifiedDate") LocalDateTime lastModifiedDate
+
+) {
+}
