fb5a20663d33fb98382d24b1c60f19bf44d51d82
woohyeon <woohyeon6885@gmail.com>
2025-09-21 14:38:03 +0900

    feat: FileService 첨부 파일 다운로드 툴 추가

--- src/main/java/com/hong/PostService_MCP_Server/service/FileService.java
+++ src/main/java/com/hong/PostService_MCP_Server/service/FileService.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
+import java.io.File;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 
@@ -51,23 +53,30 @@ public class FileService {
         }
     }
 
-    @Tool(description = "다운 받을 파일의 id를 받아, 해당 파일의 다운로드 용 presignedUrl을 발급한다.")
-    public DownloadUrlResponse getDownloadPresignedUrl(
+    @Tool(description = "다운 받을 파일의 id를 받아, 먼저 해당 파일의 다운로드 용 presignedUrl을 발급받고 그리고 발급받은 url로 파일을 다운받는다.")
+    public File downloadFile(
             String authorization,
             @ToolParam(description = "첨부된 파일들을 다운받을 게시글 id") Long fileId
     ) {
 
+        DownloadUrlResponse response = null;
+
         try {
-            DownloadUrlResponse response = restClient
+            response = restClient
                     .post()
-                    .uri("/{fileId}/download-urls", fileId)
+                    .uri("/{fileId}/download-url", fileId)
                     .header("Authorization", authorization)
                     .retrieve()
                     .body(DownloadUrlResponse.class);
-
-            return response;
         } catch (RestClientException e) {
             throw new RuntimeException("다운로드 url 발급 실패: " + e.getMessage(), e);
         }
+
+        return RestClient
+                .create()
+                .get()
+                .uri(URI.create(response.downloadUrl()))
+                .retrieve()
+                .body(File.class);
     }
 }
